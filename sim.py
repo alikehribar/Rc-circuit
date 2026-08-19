5d1657ecfbc63aeec7a83e9b3aa03903cabadf15
alikehribar <yagizalikehribar5561@gmail.com>
2026-08-19 15:46:52 +0300

    sim.py adjusted

--- sim.py
+++ sim.py
@@ -20,26 +20,7 @@ for i in range(1, len(t)):
     vout[i] = (target + (vout[i-1] - target) * a)
 
 
-# edges = np.where(np.diff(v_in) < 0)[0]
-# k = edges[0]
-# V0 = vout[k]
 
-# print("edge index k =", k)
-# print("V0 =", V0)
-
-# start = (k + 1)
-# seg = vout[start:(start + 2000)]
-# mask = (seg > (0.05 * V0))
-# v_win = seg[mask]
-# t_local = (np.arange(len(v_win)) * ts)
-
-# y = np.log(v_win / V0)
-# m, b = np.polyfit(t_local, y, 1)
-# tau_measured = -1 / m
-
-# print("tau (R*C)   =", tau * 1e6, "us")
-# print("tau_measured =", tau_measured * 1e6, "us")
-# print("error =", ((tau_measured - tau) / tau) * 100, "%")
 
 plt.plot((t * 1e6), v_in, label='Vin')
 plt.plot((t * 1e6), vout, label='Vout')
